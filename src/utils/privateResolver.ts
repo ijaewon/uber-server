d3f7cee47883eea9644b07b61dc704bbe86dec39
이재원 <jwlee@ijaewon-ui-MacBook-Pro.local>
2019-03-05 21:19:41 +0900

    applied currying programming

---
+++ src/utils/privateResolver.ts
@@ -0,0 +1,14 @@
+const privateResolver = resolverFunction => async (
+    parent: any,
+    args: any,
+    context: any,
+    info: any
+  ) => {
+    if (!context.req.user) {
+      throw new Error("No JWT. I refuse to proceed");
+    }
+    const resolved = await resolverFunction(parent, args, context, info);
+    return resolved;
+  };
+  
+  export default privateResolver;
